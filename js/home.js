fdb2b7342876c7ddecb03627a2b00709aae98c5c
violachyu <pinckychyu@gmail.com>
2025-05-11 04:31:42 -0400

    Swipe right raw

--- js/home.js
+++ js/home.js
@@ -44,6 +44,30 @@
 				}, 300);
 			});
 		});
+
+		var left = document.querySelector(".left");
+		var right = document.querySelector(".right");
+		var container = document.querySelector(".swipebox");
+
+		if (left && right && container) {
+			left.addEventListener("mouseenter", function() {
+				container.classList.add("hover-left");
+			});
+
+			// Uncomment if you want the effect to disappear on mouse leave
+			// left.addEventListener("mouseleave", function() {
+			//   container.classList.remove("hover-left");
+			// });
+
+			right.addEventListener("mouseenter", function() {
+				container.classList.add("hover-right");
+			});
+
+			// Uncomment if you want the effect to disappear on mouse leave
+			// right.addEventListener("mouseleave", function() {
+			//   container.classList.remove("hover-right");
+			// });
+		}
 	}
 
 	init();
